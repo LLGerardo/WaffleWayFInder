dc94ed5ef405b44ebfed1e5065057d7354453c0c
LLGerardo <lauralgerardo@gmail.com>
2023-10-24 20:56:54 -0500

    added some js for a back to top button

--- faq.js
+++ faq.js
@@ -1,1 +1,15 @@
 //faq javascript file
+const button = document.getElementById('back-to-top-button');
+
+button.addEventListener('click', () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
+});
